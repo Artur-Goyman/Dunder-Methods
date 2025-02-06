c7f4b0d676a4161dfb000510ba58bdc3bab0dd47
Artur Goiman <68334167+Artur-Goyman@users.noreply.github.com>
2025-02-07 01:39:49 +0300

    Add files via upload

---
+++ dunder_methods_goiman.py
@@ -0,0 +1,58 @@
+class Person:
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return f'Person: {self.name}, Age: {self.age}'
+
+    def __eq__(self, other):
+        return self.name == other.name and self.age == other.age
+
+    def __lt__(self, other):
+        return self.age < other.age
+
+    def __gt__(self, other):
+        return self.age > other.age
+
+
+p = Person("Max", 27)
+print(p)
+
+p1 = Person("Max", 27)
+p2 = Person("Maria", 56)
+p3 = Person("Max", 27)
+print(p1 == p3)
+print(p1 == p2)
+print(p1 < p2)
+print(p1 > p2)
+print(p2 > p1)
+
+
+class Collection:
+    def __init__(self, people: list):
+        self.people = people
+
+    def __add__(self, other):
+        self.people.append(other)
+        return self
+
+    def __len__(self):
+        return len(self.people)
+
+    def __getitem__(self, index):
+        return self.people[index]
+
+    def __call__(self):
+        return [x.name for x in self.people]
+
+
+c = Collection([Person("Max", 27), Person("Lewis", 41)])
+print(len(c))
+c = c + Person("Charles", 25)
+print(len(c))
+
+
+print(c[0])
+print(c())
+
